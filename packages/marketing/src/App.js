65cec76a8ab01d91f3bbaee08287639229b1fbae
Jason <jasonteo_95@hotmail.com>
2022-03-09 15:06:42 +0800

    prefixed css for prod

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'ma', // instead of generating jss, it will be ma
+})
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      {/* StylesProvider is used to customise CSS and JS generation stuff */}
+      <StylesProvider generateClassName={generateClassname}> 
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing}></Route>
